d6f9967124357c6ba654761d80d0fc4b7d0c2dcb
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-05-19 15:32:14 +0200

    ready to share

--- articles/views.py
+++ articles/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def article_by_slug(request, slug):
-    article = Article.objects.get(slug=slug)
+    article = Article.objects.filter(slug=slug).last()
     sources = article.sources.all()
     context = {'article':article, 'sources':sources}
     return render(request, "article_index.html", context)
@@ -21,19 +21,19 @@ def article_by_id(request, id):
 
 @login_required
 def unpublish_article(request, slug):
-    article = Article.objects.get(slug=slug)
+    article = Article.objects.filter(slug=slug).last()
     article.published = False
     article.save()
     return redirect('/')
 
 @login_required
 def edit_article(request, slug):
-    article = Article.objects.get(slug=slug)
+    article = Article.objects.filter(slug=slug).last()
     return redirect('http://localhost:8000/admin/articles/article/'+str(article.id)+'/change/')
 
 @login_required
 def next_article(request, slug):
-    article = Article.objects.get(slug=slug)
+    article = Article.objects.filter(slug=slug).last()
     i = 1
     found = Article.objects.filter(pk=article.id + i).exists()
     if found:
