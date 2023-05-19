efa5b7c3c716c62820345dbaaf58c82d4dd97152
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-05-19 14:58:55 +0200

    fixed error with openai key

--- newssources/views.py
+++ newssources/views.py
@@ -9,36 +9,34 @@ def save_news(url):
     #Fetching headlines
     r = requests.get(url)
     response = r.json()
-    if response["status"] != "ok":
-        print(response["message"])
-    else:
-        for article in response['articles']:
-            if Source.objects.filter(url = article['url']):
-                pass
-            else:
-                newsource = Source(sourceid = article['source']['id'],
-                                sourcename = article['source']['name'],
-                                author = article['author'],
-                                title = article['title'],
-                                description = article['description'],
-                                urltoimage = article['urlToImage'],
-                                url = article['url'],
-                                publishedAt = article['publishedAt'],
-                                content = article['content']
-                                )
-                newsource.save()
+    print(response['numResults'], ' articles found')
+    for article in response['articles']:
+        if Source.objects.filter(url = article['url']):
+            pass
+        else:
+            newsource = Source(sourceid = article['articleId'], 
+                            sourcename = article['source']['domain'],
+                            author = article['authorsByline'],
+                            title = article['title'],
+                            description = article['description'],
+                            urltoimage = article['imageUrl'],
+                            url = article['url'],
+                            publishedAt = article['pubDate'],
+                            content = article['content']
+                            )
+            newsource.save()
 
 @login_required
 def get_news(request):
+    #APIKEY = NEWS_API_KEY
     i = 1
     go = True
     while go:
-        url =  'https://newsapi.org/v2/top-headlines?country=us&pageSize=99&page='+str(i)+'&apiKey='+NEWS_API_KEY
-        #url = 'https://api.goperigon.com/v1/all?apiKey='+NEWS_API_KEY+'&from=2023-05-16&to=2023-05-17&language=en&sourceGroup=top10&showNumResults=true&showReprints=false&excludeLabel=Non-news&excludeLabel=Opinion&excludeLabel=PaidNews&excludeLabel=Roundup&excludeLabel=PressRelease&sortBy=date&page='+str(i)
+        goperigon = 'https://api.goperigon.com/v1/all?apiKey='+NEWS_API_KEY+'&from=2023-05-16&to=2023-05-17&language=en&sourceGroup=top10&showNumResults=true&showReprints=false&excludeLabel=Non-news&excludeLabel=Opinion&excludeLabel=PaidNews&excludeLabel=Roundup&excludeLabel=PressRelease&sortBy=date&page='+str(i)
         print(i)
-        save_news(url)
+        save_news(goperigon)
         i = i+1
-        if i == 5:
+        if i == 100:
             go == False
 
     return HttpResponse(204)
