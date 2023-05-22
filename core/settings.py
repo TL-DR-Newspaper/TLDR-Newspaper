3bdd740fa3662f24dc165f858b497c634fd0c91e
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-05-22 10:09:04 +0200

    added restframework dependancy

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'rest_framework',
     'articles',
     'homepage',
     'machinelearning',
